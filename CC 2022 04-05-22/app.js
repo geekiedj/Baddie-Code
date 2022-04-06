cfe1b881f35db8a8167a5cdaf4287312b54e3de9
geekiedj <barbarannegarnet@gmail.com>
2022-04-07 01:44:32 +1400

    Create app.js
    solution to challenge using the reverse and join method to solve this problem.
    04-05-22

---
+++ CC 2022 04-05-22/app.js
@@ -0,0 +1,20 @@
+//Convert number to reversed array of digits 
+
+//Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+//Example:
+
+//348597 =>[7,9,5,8,4,3]
+//0 =>[0]
+
+//solution:
+
+//First approach would be declaring the variable;
+
+let digit =['3','4','8','5','9','7']
+
+//thereafter I used the reverse method to reverse the order in the array
+  digit.reverse('')
+// I also used the join method to join the reversed order
+  digit.join('')
+
